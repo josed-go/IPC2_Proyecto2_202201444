173c8499a4b106aeca8a952d408da3d297c6b9c9
José Góngora <jdavidgongorao@gmail.com>
2023-09-14 22:58:56 -0600

    Se agregaron colores

--- interfaz/ayuda.py
+++ interfaz/ayuda.py
@@ -5,7 +5,7 @@ class ayuda:
     def __init__(self, frame):
         self.frame = frame
 
-        self.ventana = tk.Toplevel(self.frame)
+        self.ventana = tk.Toplevel(self.frame, bg="#D7EEF5")
 
         #self.ventana.geometry("850x450")
 
@@ -15,22 +15,22 @@ class ayuda:
 
         self.ventana.title("Ayuda")
 
-        label = tk.Label(self.ventana, text="Información sobre el programa", font=self.titulo)
+        label = tk.Label(self.ventana, text="Información sobre el programa", font=self.titulo, bg="#D7EEF5")
         label.pack(side="top", fill="x", pady=10)
 
-        label1 = tk.Label(self.ventana, text="Datos del estudiante:", font = self.fuente1)
+        label1 = tk.Label(self.ventana, text="Datos del estudiante:", font = self.fuente1, bg="#D7EEF5")
         label1.pack(side="top", fill="x", pady=10)
         
-        label2 = tk.Label(self.ventana, text="-> José David Góngora Olmedo\n-> 202201444\n-> Introducción a la Programación y Computación 2 sección ""D""\n-> Ingenieria en Ciencias y Sistemas\n-> 4to Semestre", font = self.fuente2)
+        label2 = tk.Label(self.ventana, text="-> José David Góngora Olmedo\n-> 202201444\n-> Introducción a la Programación y Computación 2 sección ""D""\n-> Ingenieria en Ciencias y Sistemas\n-> 4to Semestre", font = self.fuente2, bg="#D7EEF5")
         label2.pack(side="top", fill="x", pady=10)
 
-        label3 = tk.Label(self.ventana, text="Documentación", font = self.fuente1)
+        label3 = tk.Label(self.ventana, text="Documentación", font = self.fuente1, bg="#D7EEF5")
         label3.pack(side="top", fill="x", pady=10)
 
-        label4 = tk.Label(self.ventana, text="lolo", font = self.fuente2)
+        label4 = tk.Label(self.ventana, text="lolo", font = self.fuente2, bg="#D7EEF5")
         label4.pack(side="top", fill="x", pady=10)
         
         button = tk.Button(self.ventana, text="Regresar",
-            highlightbackground='black', height= 4, width=10, command=self.ventana.destroy)
+            highlightbackground='black', height= 4, width=10, command=self.ventana.destroy, bg="#ebf7fa", activebackground="#aeddeb")
         button.pack(anchor="s", side="left")
         
--- interfaz/main_page.py
+++ interfaz/main_page.py
@@ -8,6 +8,8 @@ class main_page:
     def __init__(self, raiz):
         self.raiz = raiz
 
+        self.raiz.config(bg="#D7EEF5")
+
         self.titulo = font.Font(family='Helvetica', size=25, weight="bold", slant="italic")
         self.fuente = font.Font(family='Helvetica', size=15)
 
@@ -29,16 +31,16 @@ class main_page:
 
         self.raiz.config(menu=self.menu)"""
 
-        self.label = tk.Label(self.raiz, text="Encriptador", font = self.titulo)
+        self.label = tk.Label(self.raiz, text="Encriptador", font = self.titulo, bg="#c3e5f0")
         self.label.pack(side="top", fill="x", pady=10)
 
-        self.button1 = tk.Button(self.raiz, text="Cargar archivo", highlightbackground='black', height= 2, width=25, padx=10, pady=10, font = self.fuente)
-        self.button2 = tk.Button(self.raiz, text="Generar archivo", highlightbackground='black', height= 2, width=25 ,padx=10, pady=10, font = self.fuente)
-        self.button3 = tk.Button(self.raiz, text="Gestión de drones",highlightbackground='black', height= 2, width=25 ,padx=10, pady=10, font = self.fuente)
-        self.button4 = tk.Button(self.raiz, text="Gestión de sitemas de drones", highlightbackground='black', height=2, width=25 ,padx=10, pady=10, font = self.fuente)
-        self.button7 = tk.Button(self.raiz, text="Gestión de mensajes", highlightbackground='black', height=2, width=25 ,padx=10, pady=10, font = self.fuente)
-        self.button5 = tk.Button(self.raiz, text="Ayuda", highlightbackground='black', height= 5, width=10, font = self.fuente, command = self.mostrar_ayuda)
-        self.button6 = tk.Button(self.raiz, text="Inicializar", highlightbackground='black', height= 5, width=10, font = self.fuente)
+        self.button1 = tk.Button(self.raiz, text="Cargar archivo", highlightbackground='black', height= 2, width=25, padx=10, pady=10, font = self.fuente, bg="#ebf7fa", activebackground="#aeddeb")
+        self.button2 = tk.Button(self.raiz, text="Generar archivo", highlightbackground='black', height= 2, width=25 ,padx=10, pady=10, font = self.fuente, bg="#ebf7fa", activebackground="#aeddeb")
+        self.button3 = tk.Button(self.raiz, text="Gestión de drones",highlightbackground='black', height= 2, width=25 ,padx=10, pady=10, font = self.fuente, bg="#ebf7fa", activebackground="#aeddeb")
+        self.button4 = tk.Button(self.raiz, text="Gestión de sitemas de drones", highlightbackground='black', height=2, width=25 ,padx=10, pady=10, font = self.fuente, bg="#ebf7fa", activebackground="#aeddeb")
+        self.button7 = tk.Button(self.raiz, text="Gestión de mensajes", highlightbackground='black', height=2, width=25 ,padx=10, pady=10, font = self.fuente, bg="#ebf7fa", activebackground="#aeddeb")
+        self.button5 = tk.Button(self.raiz, text="Ayuda", highlightbackground='black', height= 5, width=10, font = self.fuente, command = self.mostrar_ayuda, bg="#ebf7fa", activebackground="#aeddeb")
+        self.button6 = tk.Button(self.raiz, text="Inicializar", highlightbackground='black', height= 5, width=10, font = self.fuente, bg="#ebf7fa", activebackground="#aeddeb")
 
         self.button1.pack()
         self.button2.pack()
